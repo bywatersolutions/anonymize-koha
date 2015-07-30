8ef8a3ed12b8b16c3137d665f7f50f3ba40eef8a
Kyle M Hall <kyle@bywatersolutions.com>
2015-07-30 06:45:08 -0700

    Updates

--- anonymize_koha.pl
+++ anonymize_koha.pl
@@ -2,33 +2,34 @@
 
 use Modern::Perl;
 
-use Data::Random::WordList;
+#use Data::Random::WordList;
 use Data::Dumper;
 
+use C4::Context;
 use Koha::Database;
 
-our $dbh = C4::Context->dbh();
+our $dbh    = C4::Context->dbh();
 our $schema = Koha::Database->new()->schema();
 my $rs = $schema->resultset('Borrower');
 
 my @columns = $rs->result_source->columns;
-shift @columns; #get rid of borrowernumber
+@columns = grep( !/borrowernumber/, @columns );
+@columns = grep( !/cardnumber/,     @columns );
 
 say "Starting...";
 while ( my $b = $rs->next() ) {
-    map { $b->set_column( $_ => get_random( $_ ) ) } @columns;
+    map { $b->set_column( $_ => get_random($_) ) } @columns;
     $b->update();
     say "Updated borrower " . $b->borrowernumber();
-    die();
 }
 
 sub get_random {
     my ($column) = @_;
 
     my $query = "SELECT $column FROM borrowers ORDER BY RAND() LIMIT 1";
-    my $sth = $dbh->prepare( $query );
+    my $sth   = $dbh->prepare($query);
     $sth->execute();
-    my $hr = $sth->fetchrow_hashref();
+    my $hr    = $sth->fetchrow_hashref();
     my $value = $hr->{$column};
     return $value;
 }
